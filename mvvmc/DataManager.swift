1dc6b544d2339dd5caf4e4bac93ed4ff7996813e
Mikail Freitas <mikailcf@gmail.com>
2017-01-02 15:02:52 -0200

    beginning model layers

---
+++ mvvmc/DataManager.swift
@@ -0,0 +1,15 @@
+//
+//  DataManager.swift
+//  mvvmc
+//
+//  Created by Mikail Freitas on 02/01/17.
+//  Copyright © 2017 Mikail. All rights reserved.
+//
+
+import Foundation
+
+class DataManager {
+    static func getLocalPersonsList() -> [Person] {
+
+    }
+}
---
+++ mvvmc/Person.swift
@@ -0,0 +1,27 @@
+//
+//  Person.swift
+//  mvvmc
+//
+//  Created by Mikail Freitas on 02/01/17.
+//  Copyright © 2017 Mikail. All rights reserved.
+//
+
+import Foundation
+
+struct Person {
+    var firstName: String
+    var lastName: String
+    var nickname: String
+    var cpf: Int
+    var phoneNumber: Int
+    var birthDate: Date
+
+    init(mockParameter: Int) {
+        self.firstName = "Nome\(mockParameter)"
+        self.lastName = "Sobrenome\(mockParameter)"
+        self.nickname = "pessoa\(mockParameter)"
+        self.cpf = 10000000000 + mockParameter
+        self.phoneNumber = 11000000000 + mockParameter
+        self.birthDate = Date()
+    }
+}
